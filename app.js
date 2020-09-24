2b9e903175a783d21ceef3d594fd854dc47aaee5
umeshramya <umeshbilagi@gmail.com>
2020-09-24 18:03:54 +0530

    done

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
 const express = require('express');
 const app = express();
+const socketIO = require("socket.io");
 const path = require('path');
 const router = express.Router();
+const server = app.listen(process.env.port || 3000);
+
+const io = socketIO(server)
 
 app.use(express.static('public'))
-// app.use(express.static('files'))
 
-router.get('/',function(req,res){
-res.render("index.html")
+app.get("/", (req, res)=>{
+    res.render("index.html")
+}
+   
+)
+
+let broadcaster
+
+io.sockets.on("connection", socket => {
+  socket.on("broadcaster", () => {
+    broadcaster = socket.id;
+    socket.broadcast.emit("broadcaster");
+  });
+  socket.on("watcher", () => {
+    socket.to(broadcaster).emit("watcher", socket.id);
+  });
+  socket.on("disconnect", () => {
+    socket.to(broadcaster).emit("disconnectPeer", socket.id);
+  });
+});
+
+
+socket.on("offer", (id, message) => {
+    socket.to(id).emit("offer", socket.id, message);
+});
+socket.on("answer", (id, message) => {
+  socket.to(id).emit("answer", socket.id, message);
+});
+socket.on("candidate", (id, message) => {
+  socket.to(id).emit("candidate", socket.id, message);
 });
 
 
 
-//add the router
-app.use('/', router);
-app.listen(process.env.port || 3000);
 
 console.log('Running at Port 3000');
